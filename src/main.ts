0796311520c131881fcbfc64ea35eda511f12ce2
caionobrex <marialoucos@hotmail.com.br>
2023-02-09 16:55:26 -0300

    hybrid app

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,16 @@
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
-import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { Transport } from '@nestjs/microservices';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
-    AppModule,
-    {
-      transport: Transport.TCP,
-      options: { host: '0.0.0.0', port: 3004 },
-    },
-  );
-  await app.listen();
+  const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
+  app.connectMicroservice({
+    transport: Transport.TCP,
+    options: { port: configService.get<number>('PORT') },
+  });
+  await app.startAllMicroservices();
+  await app.listen(configService.get<number>('PORT'));
 }
 bootstrap();
